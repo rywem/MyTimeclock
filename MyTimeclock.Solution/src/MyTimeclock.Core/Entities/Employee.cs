455bb99ee68f903f265cdf51dcc5377b60771d78
Ryan <cybervoid41@gmail.com>
2020-11-07 10:49:08 -0800

    Added employee entity.

---
+++ MyTimeclock.Solution/src/MyTimeclock.Core/Entities/Employee.cs
@@ -0,0 +1,13 @@
+﻿using MyTimeclock.SharedKernal;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyTimeclock.Core.Entities
+{
+    public class Employee : BaseEntity
+    {
+        public decimal HourlyRate { get; set; }
+        public DateTime StartDate { get; set; }
+    }
+}
