2324b441908c924c4e5d53491063745c83ad6fa2
Ryan <cybervoid41@gmail.com>
2020-11-07 12:16:53 -0800

    Shift Entity created

---
+++ MyTimeclock.Solution/src/MyTimeclock.Core/Entities/Shift.cs
@@ -0,0 +1,14 @@
+﻿using MyTimeclock.Core.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyTimeclock.Core.Entities
+{
+    public class Shift
+    {
+        public int EmployeeId { get; set; }
+        public TimeEntry TimeIn { get; set; }
+        public TimeEntry TimeOut { get; set; }
+    }
+}
