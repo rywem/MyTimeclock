b65b14ede667a66a153c85015fe8bf13814d4a39
Ryan <cybervoid41@gmail.com>
2020-11-07 10:38:29 -0800

    SharedKernal initial project setup

---
+++ MyTimeclock.Solution/src/MyTimeclock.SharedKernal/Interfaces/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using MyTimeclock.SharedKernal;
+namespace MyTimeclock.SharedKernal.Interfaces
+{
+    public interface IRepository
+    {
+        Task<T> GetByIdAsync<T>( int id ) where T : BaseEntity;
+        Task<T> FirstOrDefaultAsync<T>( Expression<Func<T, bool>> filter ) where T : BaseEntity;
+        Task<List<T>> ListAsync<T>() where T : BaseEntity;
+        Task<List<T>> ListAsync<T>( Expression<Func<T, bool>> filter ) where T : BaseEntity;        
+        Task<T> AddAsync<T>( T entity ) where T : BaseEntity;
+        Task UpdateAsync<T>( T entity ) where T : BaseEntity;
+        Task DeleteAsync<T>( T entity ) where T : BaseEntity;
+    }
+}
